ec86f2bfd056371afe7b9c98e81b7066db519605
Laeti-Create <l.lelaumier@gmail.com>
2026-06-26 19:31:46 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,11 @@
 // Ce bloc n'est pas un mot de passe : il sert à identifier ton projet côté navigateur.
 
 export const firebaseConfig = {
-  apiKey: "REMPLACE_MOI",
-  authDomain: "REMPLACE_MOI.firebaseapp.com",
-  projectId: "REMPLACE_MOI",
-  storageBucket: "REMPLACE_MOI.appspot.com",
-  messagingSenderId: "REMPLACE_MOI",
-  appId: "REMPLACE_MOI"
+  apiKey: "AIzaSyCz61po0m3qVv09QlPhY6_q7D95UFY53yY",
+  authDomain: "laeti-fit-tracker.firebaseapp.com",
+  projectId: "laeti-fit-tracker",
+  storageBucket: "laeti-fit-tracker.firebasestorage.app",
+  messagingSenderId: "536939261716",
+  appId: "1:536939261716:web:271c719ec09169f6c89c01",
+  measurementId: "G-8PM7H3TEC1"
 };
