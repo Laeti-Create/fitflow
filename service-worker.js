c4ac2b1f42644d9071ae8b98ae7b44d3ab1d6fe2
Laeti-Create <l.lelaumier@gmail.com>
2026-07-14 18:04:54 +0200

    Bump v51 test cache for offline badge layout

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "fitflow-cache-v51-app-shell-2";
-const APP_SHELL_VERSION = "0.51.0-develop.2";
+const CACHE_NAME = "fitflow-cache-v51-app-shell-3";
+const APP_SHELL_VERSION = "0.51.0-develop.3";
 
 const CORE_ASSETS = [
   "/fitflow/",
