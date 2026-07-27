83b56de5a395900dd9459b420431979b07d818d5
Laeti-Create <l.lelaumier@gmail.com>
2026-07-27 11:23:36 +0200

    Rotate PWA cache for 0.53.0-develop.3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_SHELL_VERSION = "0.53.0-develop.2";
+const APP_SHELL_VERSION = "0.53.0-develop.3";
 const SCOPE_URL = new URL(self.registration.scope);
 const BASE_PATH = SCOPE_URL.pathname.endsWith("/") ? SCOPE_URL.pathname : `${SCOPE_URL.pathname}/`;
 const CACHE_SCOPE = BASE_PATH.replace(/^\/+|\/+$/g, "").replaceAll("/", "-") || "root";
